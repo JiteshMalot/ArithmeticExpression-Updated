48646302ad6cf3c6469dc4fd136d28b81fb17c80
JiteshMalot <62233584+JiteshMalot@users.noreply.github.com>
2020-04-07 14:07:37 +0530

    Update ArithmeticExpressionApplicationTests.java

--- src/test/java/com/demo/ArithmeticExpressionApplicationTests.java
+++ src/test/java/com/demo/ArithmeticExpressionApplicationTests.java
@@ -6,8 +6,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ArithmeticExpressionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private ArithmeticService arithmeticService;
 
+	@Test
+	void testExpression() {
+
+		// Test 1
+		ArithmeticExpressionEntity expEntity = new ArithmeticExpressionEntity();
+		expEntity.setExpression("(1+(3-2))");
+		try {
+			int res = arithmeticService.evaluateExpression(expEntity);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
